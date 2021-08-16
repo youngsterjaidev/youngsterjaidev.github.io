3a5a9ce258e61adbb11f41064bc588ef9515ef70
Youngster Jaidev <jaidevv999@gmail.com>
2021-08-16 17:57:29 +0530

    Add the type

---
+++ floIntern/js/main.ts
@@ -0,0 +1,362 @@
+type Path = {
+    current: string,
+    printPath: () => void
+}
+
+((ready: boolean): void => {
+    if (ready) {
+        console.log("JS On Fire");
+
+        let _input: HTMLInputElement = document.querySelector("#input");
+
+        _input.addEventListener("input", (e: MouseEvent) => {
+            let val = e.target.value;
+
+            console.log(val);
+
+            if (finalList.length !== 0) {
+                const list = finalList.filter((element): boolean => {
+                    let newUserName: string = element.name.slice(0, val.length);
+                    return newUserName.toLowerCase() === val.toLowerCase();
+                });
+
+                console.log(list);
+
+                if (list.length) {
+                    //
+                    let el: HTMLDivElement= document.createElement("div");
+                    for (let i of list) {
+                        let card: HTMLAnchorElement = document.createElement("a");
+                        card.classList.add("card");
+                        card.href = `#${i.floId}`;
+                        card.innerHTML = `
+                        <h3>${i.name}</h3>
+                        <h5>${i.floId}</h5>
+                    `;
+                        el.appendChild(card);
+                    }
+                    _rootDiv.innerHTML = el.innerHTML;
+                } else {
+                    let el: HTMLDivElement = document.createElement("div");
+                    for (let i of finalList) {
+                        let card: HTMLAnchorElement = document.createElement("a");
+                        card.classList.add("card");
+                        card.href = `#${i.floId}`;
+                        card.innerHTML = `
+                        <h3>${i.name}</h3>
+                        <h5>${i.floId}</h5>
+                    `;
+                        el.appendChild(card);
+                    }
+                    _rootDiv.innerHTML = el.innerHTML;
+                }
+            } else {
+                return `<div>Loading</div>`;
+            }
+        });
+
+        // intern Data "const internList = []"
+        //
+        // Data from distributer "const distributerData = []"
+        //
+        // filter the transaction of interns from the distributer
+
+        const receiverList: any[] = [];
+
+        const internList: any[] = [];
+
+        const finalList: any[] = [];
+
+        customElements.define(
+            "my-card",
+            class MyCard extends HTMLElement {
+                displayUsername: HTMLElement
+                displayUserId: HTMLElement
+                
+                // look all the value we have to look on change
+                static get observedAttributes() {
+                    return ["username", "userid"];
+                }
+
+                constructor() {
+                    super();
+                    // attach the shadow DOM
+                    this.attachShadow({ mode: "open" });
+                }
+
+                get username() {
+                    return this.getAttribute("username");
+                }
+
+                get userid() {
+                    return this.getAttribute("userid");
+                }
+
+                set username(val) {
+                    this.setAttribute("username", val);
+                }
+
+                set userid(val) {
+                    this.setAttribute("userid", val);
+                }
+
+                async fetchTokenInfo(): Promise<void> {
+                    console.log(this.userid)
+                    try {
+                    let r = await fetch(`https://ranchimallflo.duckdns.org/api/v1.0/getFloAddressDetails?floAddress=${this.userid.slice(1)}`)
+
+                    console.log(r)
+                    } catch(e) {
+                        console.log("Error Occured ", e)
+                    }
+                }
+
+                attributeChangedCallback(name, oldValue, newValue) {
+                    if (oldValue !== newValue && this.displayUsername) {
+                        console.log("----------", this.userid);
+                        this.displayUsername.innerText = this.username;
+                        this.displayUserId.innerText = this.userid;
+                    }
+                }
+
+                // run when the element attached to the DOM
+                connectedCallback() {
+                    const template = document.createElement("template");
+                    template.innerHTML = this.render();
+                    const node = document.importNode(template.content, true);
+                    this.shadowRoot.append(node);
+
+                    // select the element from the shadow DOM and set the value
+                    this.displayUsername = this.shadowRoot.querySelector(
+                        ".username"
+                    );
+                    this.displayUserId = this.shadowRoot.querySelector(
+                        ".userid"
+                    );
+
+                    this.fetchTokenInfo()
+                }
+
+                // render the element to the DOM
+                render(): string {
+                    if (finalList.length) {
+                        let el: HTMLElement = document.createElement("div");
+
+                        console.log(this.userid.slice(1));
+
+                        const myResult = finalList.filter((l) => {
+                            return this.userid.slice(1) === l.floId;
+                        });
+
+                        console.log(myResult);
+
+                        myResult.forEach((r) => {
+                            let username: HTMLHeadElement = document.createElement("h2");
+                            username.innerText = r.name;
+                            username.style.textAlign = "center";
+                            let txData: HTMLUListElement = document.createElement("ul");
+                            if(r.transactions.length) {
+                            r.transactions.forEach((t) => {
+                                let li: HTMLLIElement = document.createElement("li");
+                                li.style.margin = "1em 0em";
+                                li.innerText = t.transaction.floData;
+                                txData.appendChild(li);
+                            });
+                            } else {
+                                let li:HTMLLIElement = document.createElement("div");
+                                li.innerText = "No Transaction Found"
+                                txData.appendChild(li);
+                            }
+                            el.appendChild(username);
+                            el.appendChild(txData);
+                        });
+
+                        console.log(el);
+
+                        return el.innerHTML;
+                    }
+                }
+            }
+        );
+
+        let path = {
+            current: window.location.hash || "#",
+            printPath: () => {
+                console.log("The current Path", this.current);
+            },
+        };
+
+        const _rootDiv: HTMLElement = document.getElementById("uInfo");
+
+        // render all the list of the use on to the DOM
+        function renderList() {
+            console.log("==============", receiverList.length);
+            if (finalList.length !== 0) {
+                let el: HTMLDivElement = document.createElement("div");
+                for (let i of finalList) {
+                    let card: HTMLAnchorElement = document.createElement("a");
+                    card.classList.add("card");
+                    card.href = `#${i.floId}`;
+                    card.innerHTML = `
+                        <h3>${i.name}</h3>
+                        <h5>${i.floId}</h5>
+                    `;
+                    el.appendChild(card);
+                }
+                return el.innerHTML;
+            } else {
+                return `<div>Loading</div>`;
+            }
+        }
+
+        // render the details page 
+        function renderDetail() {
+            return `
+                <my-card username="red" style="flex: 1;" userid="${window.location.hash}"></my-card>
+            `;
+        }
+
+        let routes = {
+            "#": renderList(),
+            "#detail": renderDetail(),
+        };
+
+        /**
+         * Run when there is change of hash on the window
+         * - get the first value from the routes object
+         * - check the value is matching to the comming route value
+         * - if so, render the home page otherwise detail page
+         */
+        const handleRender = (route) => {
+            // ...
+            let val = Object.keys(routes)[0];
+            if (val === route) {
+                _rootDiv.innerHTML = renderList();
+                console.log(receiverList.length);
+            } else {
+                _rootDiv.innerHTML = renderDetail();
+            }
+        };
+
+        // check the change in hash
+        window.addEventListener("hashchange", (e) => {
+            // get the current path
+            path.current = window.location.hash || "#";
+
+            /**
+             * @param always be the path of the window
+             */
+            handleRender(path.current);
+        });
+
+        // render the home page default
+        _rootDiv.innerHTML = renderList();
+
+        /**
+         * Creating a list in which store all the
+         * flo addresses of the receiver
+         */
+
+        /**
+         * get the intern data from the RanchiMall
+         * - request the server for data
+         * - loop over the response 
+         * - push the each data to the "finalList arrary"
+         * - call the fetchInternData()
+         */
+        async function getInternData(): Promise<void> {
+            try {
+                let r = await floCloudAPI.requestObjectData("RIBC", {
+                    application: "RIBC",
+                    receiverID: "FMeiptdJNtYQEtzyYAVNP8fjsDJ1i4EPfE",
+                    senderIDs: [
+                        "F7TxecSPV8oFZE6Y2giVuP8hfsqfAD6erj",
+                        "FCja6sLv58e3RMy41T5AmWyvXEWesqBCkX",
+                        "FPFeL5PXzW9bGosUjQYCxTHSMHidnygvvd",
+                        "FS4jMAcSimRMrhoRhk5cjuJERS2otiwq4A",
+                        "FU2fkubqGD5ynbr7qhvaPe9DPqrNdGB6mw",
+                        "FUkY9k9mVVxPzYA8uUGxUuLVH6CB83Nb9r",
+                    ],
+                });
+
+                console.log("Inter: ", r)
+
+                if (r) {
+                    console.log("intern Data from the server ", r);
+                    let i = floGlobals.appObjects.RIBC.internList 
+                    for (let key in i) {
+
+                        internList.push({
+                            floId: key,
+                            floUserName: i[key],
+                        });
+
+                        console.table(i)
+                    }
+
+                    fetchInternData();
+                }
+            } catch (e) {
+                // ERROR HANDLING
+                console.log("Error Occur while fetching the Intern Data", e);
+                _rootDiv.innerHTML = `
+                    <div style="flex: 1; padding: 1em;">
+                        <h1>Flosight is not Working ! [keep Refreshing the page ...]</h1>
+                        <p style="color: red;">${e}</p>
+                    </div>
+                `
+            }
+        }
+
+        // get the internData after the 3sec I don't know why
+        setTimeout(() => {
+            getInternData();
+        }, 3000);
+
+        /**
+         * Fetch initial transactions
+         * - request the distributer transactions from  the server
+         * - push all these transactions to the "receiverList array"
+         * - then loop over to the interList array in which we collect interns data
+         * - filter out the transactions of the intern from the distributer transactions
+         * - push all the data to the new called finalList array
+         * - render the home page to the DOM
+         */
+        function fetchInternData() {
+            floBlockchainAPI
+                .readAllTxs("FThgnJLcuStugLc24FJQggmp2WgaZjrBSn", "", "")
+                .then((r) => {
+                    console.log(r);
+                    // loop over the response transactions
+                    r.forEach((user) => {
+                        console.log(user);
+                        // sending all the transaction to the new array
+                        receiverList.push({
+                            floId: user.vout[0].scriptPubKey.addresses[0],
+                            transaction: user,
+                        });
+                    });
+
+                    // loop over the intern data
+                    for (let d of internList) {
+                        // filter the intern transactions
+                        const result = receiverList.filter((i) => {
+                            return i.floId === d.floId;
+                        });
+
+                        // add all the transaction to the new Array
+                        finalList.push({
+                            name: d.floUserName,
+                            floId: d.floId,
+                            transactions: [...result],
+                        });
+                    }
+
+                    console.table(finalList);
+
+                    // re-render the DOM
+                    _rootDiv.innerHTML = renderList();
+                }, console.error);
+        }
+    }
+})(true);
